fef66af15ee214f7b1411a585c3957260b06be33
yangdong07 <yangdong07@gmail.com>
2018-08-03 13:19:53 +0800

    P2955 [USACO09OCT]奇数偶数Even? Odd?, Solved

--- noip/cpp/p2955.cpp
+++ noip/cpp/p2955.cpp
@@ -3,16 +3,20 @@
 
 
 #include <cstdio>
+#include <cstring>
+
+#define MAX_SIZE  100
 
 int main()
 {
-    int n, i;
-    int m;
+    int n, i, len;
+    char s[MAX_SIZE];
     scanf("%d", &n);
     for (i = 0; i < n; ++i)
     {
-        scanf("%d", &m);
-        printf(m & 1 ? "odd\n" : "even\n");
+        scanf("%s", s);
+        len = strlen(s);
+        printf((s[len - 1] - '0') & 1 ? "odd\n" : "even\n");
     }
 
 }
