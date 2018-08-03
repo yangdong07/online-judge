a66ccdc90eb394a3403272cb2377e2ef74552ed8
yangdong07 <yangdong07@gmail.com>
2018-08-03 09:27:20 +0800

    P4413 [COCI2006-2007#2] R2, Solved

---
+++ noip/cpp/p4413.cpp
@@ -0,0 +1,11 @@
+// P4413 [COCI2006-2007#2] R2
+// https://www.luogu.org/problemnew/show/P4413
+
+#include <cstdio>
+
+int main()
+{
+    int s, a;
+    scanf("%d %d", &a, &s);
+    printf("%d", s * 2 - a);
+}
