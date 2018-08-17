41722c5cd95ad675d16da4fa6082220b2f7b9acd
yangdong07 <yangdong07@gmail.com>
2018-08-17 12:18:37 +0800

    try p1074, advanced sudoku

---
+++ noip/p1074.cpp
@@ -0,0 +1,73 @@
+// P1074 靶形数独
+// https://www.luogu.org/problemnew/show/P1074
+
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+#define N 9
+#define NN 81
+
+int grid[NN];
+bool row[N][N + 1], col[N][N + 1], box[N][N + 1];
+int score[NN];
+
+int max_sum = -1;
+
+void print_grid()
+{
+    for (int i = 0; i < 9; i++, putchar('\n'))
+    {
+        for (int j = 0; j < 9; j++)
+            printf("%d ", grid[i * N + j]);
+    }
+}
+
+void search(int k, int sum)
+{
+    while (k < NN && grid[k] != 0) ++k;
+    if (k == NN)
+    {
+        if (sum > max_sum)
+            max_sum = sum;
+        // print_grid();
+        // printf("\n");
+        // printf("%d\n", max_sum);
+        return;
+    }
+
+    int r = k / N, c = k % N;
+    for (int i = 1; i <= 9; i++)
+        if (!row[r][i] && !col[c][i] && !box[r / 3 * 3 + c / 3][i])
+        {
+            row[r][i] = col[c][i] = box[r / 3 * 3 + c / 3][i] = 1;
+            grid[k] = i;
+            search(k + 1, sum + i * score[k]);
+            row[r][i] = col[c][i] = box[r / 3 * 3 + c / 3][i] = 0;
+            grid[k] = 0;
+        }
+}
+
+int main()
+{
+    int i, j, t;
+    int sum = 0;
+
+    int raw_score[] = {6, 7, 8, 9, 10};
+    for (i = 0; i < N; i++)
+        for (j = 0; j < N; j++)
+        {
+            scanf("%d", &t);
+            grid[i * N + j] = t;
+            score[i * N + j] = raw_score[min(min(i, N - 1 - i), min(j, N - 1 - j))];
+            if (t)
+            {
+                sum += t * score[i * N + j];
+                row[i][t] = col[j][t] = box[i / 3 * 3 + j / 3][t] = 1;
+            }
+        }
+    search(0, sum);
+    printf("%d\n", max_sum);
+    return 0;
+}
